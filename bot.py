d9b3ecaa0ba348a12852be0704c96fe306783aa5
noelsy237 <64825351+noelsy237@users.noreply.github.com>
2021-08-29 16:45:17 +1000

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,6 +6,7 @@ load_dotenv()
 token = os.getenv('DISCORD_TOKEN')
 
 client = commands.Bot(command_prefix='-')
+client.remove_command('help')
 
 @client.event
 async def on_ready():
@@ -13,6 +14,23 @@ async def on_ready():
     await client.change_presence(activity=activity)
     print('Success!')
 
+@client.command()
+async def help(ctx):
+    embed = discord.Embed(
+        title='Command List',
+        description='Try out any of the commands listed below.',
+        colour=discord.Colour.blue()
+    )
+    embed.add_field(name='Help', value='Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim 
+                    ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip 
+                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate 
+                    velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat 
+                    cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id 
+                    est laborum.', inline=False)
+
+    await ctx.send(embed=embed)    
+    
 @client.command()
 async def hello(ctx):
     chats = ["The last time I was this happy, I was told I could stop paying alimony.",
