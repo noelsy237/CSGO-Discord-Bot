35be405f70d6a614b1cc586421f515fc0c5c6e85
Micky <ad312@pm.me>
2021-08-29 17:33:47 +1000

    implement audio playback

--- bot.py
+++ bot.py
@@ -1,12 +1,15 @@
-import os, random, discord
+import os, random, discord, asyncio
 from dotenv import load_dotenv
 from discord.ext import commands
+from discord import FFmpegPCMAudio
+from discord.utils import get
+
 
 load_dotenv()
 token = os.getenv('DISCORD_TOKEN')
 
 client = commands.Bot(command_prefix='-')
-client.remove_command('help')
+
 
 @client.event
 async def on_ready():
@@ -15,28 +18,24 @@ async def on_ready():
     print('Success!')
 
 @client.command()
-async def help(ctx):
-    embed = discord.Embed(
-        title='Command List',
-        description='Try out any of the commands listed below.',
-        colour=discord.Colour.blue()
-    )
-    embed.add_field(name='Help', value='Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim 
-                    ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip 
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate 
-                    velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat 
-                    cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id 
-                    est laborum.', inline=False)
-
-    await ctx.send(embed=embed)    
-    
-@client.command()
 async def hello(ctx):
     chats = ["The last time I was this happy, I was told I could stop paying alimony.",
              "You're alive! I've never been more happy to lose fifty quid.",
              "You deserve a promotion! Well, our fundings all tied up so I can't afford to give you one but on paper, "
              "wooo, you've earned it."]
     await ctx.send(random.choice(chats))
+
+@client.command()
+async def audio(ctx):
+    channel = ctx.message.author.voice.channel
+    if not channel:
+        await ctx.send("You are not connected to a voice channel")
+        return
+    voice = get(client.voice_clients, guild=ctx.guild)
+    if voice and voice.is_connected():
+        await voice.move_to(channel)
+    else:
+        voice = await channel.connect()
+    voice.play(discord.FFmpegPCMAudio(executable=r"ffmpeg.exe", source="audio/aw_hell.mp3"))
 
 client.run(token)
