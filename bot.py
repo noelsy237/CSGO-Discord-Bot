9962e1eeadaf44ffdbd50e1f47f60707b6f3de26
Micky <ad312@pm.me>
2021-09-02 18:55:10 +1000

    add more audio clips

--- bot.py
+++ bot.py
@@ -7,6 +7,7 @@ load_dotenv()
 token = os.getenv('DISCORD_TOKEN')
 bot = commands.Bot(command_prefix='-')
 audioText = json.load(open('audio.json'))
+bot.remove_command('help')
 
 @bot.event
 async def on_ready():
@@ -14,18 +15,28 @@ async def on_ready():
     await bot.change_presence(activity=activity)
     print('Success!')
 
+
 @bot.command()
-async def hello(ctx):
+async def help(ctx):
+    await ctx.send("Hello operator! You can say hi to me by typing '-hi' followed by any of the following options```felix, legacy, hostage\n\nExample: -hi felix```")
+
+@bot.command()
+async def hi(ctx, type):
     #https://github.com/Rapptz/discord.py/blob/master/examples/basic_voice.py
-    await ctx.message.delete()
-    audio, text = random.choice(list(audioText.items()))
     if ctx.author.voice and ctx.author.voice.channel:
+        await ctx.message.delete()
         authorChannel = ctx.author.voice.channel
         if ctx.voice_client is None:
             await authorChannel.connect()
         elif ctx.voice_client.channel != authorChannel:
             await ctx.voice_client.disconnect()
             await authorChannel.connect()
+        if type is None or type == "felix":
+            audio, text = random.choice(list(audioText['felix'][0].items()))
+        elif type == "hostage":
+            audio, text = random.choice(list(audioText['hostage'][0].items()))
+        elif type == "legacy":
+            audio, text = random.choice(list(audioText['legacy'][0].items()))
         ctx.voice_client.play(discord.FFmpegPCMAudio(source=f"audio/{audio}.wav"))
         await ctx.send(text)
     else:
