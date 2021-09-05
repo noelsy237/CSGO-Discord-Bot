2e2b1fa93231633708299c3eca838f871ec67861
Micky <ad312@pm.me>
2021-09-05 22:19:10 +1000

    fix bugs

--- bot.py
+++ bot.py
@@ -1,25 +1,54 @@
-import os, random, discord, json, requests, datetime, database
+import os, random, discord, json, requests, datetime
 from dotenv import load_dotenv
 from discord.ext import commands, tasks
-from discord import FFmpegPCMAudio
-from discord import Color
+from discord import FFmpegPCMAudio, Color
 from asyncio import sleep
 from steam.steamid import SteamID
+from database import get_db
+
+# Oath2 URL:
+'''https://discord.com/api/oauth2/authorize?client_id=883904442313891900&permissions=36793344&redirect_uri=https%3A%2F%2Fdiscord.com%2Fapp
+&response_type=code&scope=identify%20bot'''
+
 
 load_dotenv()
 discordToken = os.getenv('DISCORD_TOKEN')
 steamToken = os.getenv('STEAM_TOKEN')
-vacChannelId = int(os.getenv('VAC_CHANNEL_ID'))
 bot = commands.Bot(command_prefix='-')
 audioText = json.load(open('audio.json'))
 bot.remove_command('help')
 
+
+## Events
 # Init function
 @bot.event
 async def on_ready():
     activity = discord.Game(name="Counter-Strike: Global Offensive", type=3)
     await bot.change_presence(activity=activity)
+    db = get_db()
+    for guild in bot.guilds:
+        if not db.execute('SELECT EXISTS(SELECT 1 FROM guilds WHERE id = ?)', (guild.id,)).fetchone()[0]: 
+            db.execute('INSERT INTO guilds (id, alert_channel, add_date) VALUES (?, ?, ?)', 
+                (guild.id, '0', datetime.datetime.now()))
+            db.commit()
     print('Bot started successfully')
+
+# When bot is added to a new guild
+@bot.event
+async def on_guild_join(guild):
+    db = get_db()
+    db.execute('INSERT INTO guilds (id, alert_channel, add_date) VALUES (?, ?, ?)', 
+        (guild.id, '0', datetime.datetime.now()))
+    db.commit()
+
+# When a bot is removed from a guild
+@bot.event
+async def on_guild_remove(guild):
+    db = get_db()
+    db.execute('DELETE FROM guilds WHERE id = ?', (guild.id,))
+    db.commit()
+    db.execute('DELETE FROM players WHERE guild_id = ?', (guild.id,))
+    db.commit()
 
 # Make bot leave when channel is empty
 @bot.event
@@ -30,6 +59,8 @@ async def on_voice_state_update(member, before, after):
     if len(voice_state.channel.members) == 1:
         await voice_state.disconnect()
 
+
+## Commands
 # Help command
 @bot.command()
 async def help(ctx):
@@ -40,14 +71,14 @@ async def help(ctx):
     helpEmbed.add_field(name="-vac", value="""Add a suspected cheater to a tracking list by using their profile url. 
         You can also retrieve a list of all players being tracked or banned""")
     helpEmbed.add_field(name="Options", value="[profile] [track] [ban]\n\nExample: -vac https://steamcommunity.com/id/Micky2000")
-    await ctx.channel.send(embed=helpEmbed)
+    await ctx.send(embed=helpEmbed)
 
 # Play a random audio clip from game files
 @bot.command()
 async def hi(ctx, type=None):
     #https://github.com/Rapptz/discord.py/blob/master/examples/basic_voice.py
     if ctx.author.voice and ctx.author.voice.channel:
-        await ctx.message.delete()
+        #await ctx.message.delete()
         authorChannel = ctx.author.voice.channel
         if ctx.voice_client is None:
             await authorChannel.connect()
@@ -68,7 +99,7 @@ async def hi(ctx, type=None):
 # Add a player to the tracking list for ban checking
 @bot.command()
 async def vac(ctx, input=None, playerLink=None):
-    db = database.get_db()
+    db = get_db()
     error = None
     if input:
         input.strip()
@@ -84,15 +115,14 @@ async def vac(ctx, input=None, playerLink=None):
         userId = SteamID.from_url(input)
         player = SteamID(userId)
         if not player.is_valid():
-            error = "User was not found. You must supply a community profile URL."
-        elif db.execute('SELECT EXISTS(SELECT 1 FROM players WHERE steamID = ?)', (userId,)).fetchone()[0]:
+            error = "User was not found. You must supply a valid community profile URL."
+        elif db.execute('SELECT EXISTS(SELECT 1 FROM players WHERE steam_id = ? and guild_id = ?)', (userId, ctx.guild.id)).fetchone()[0]:
             error = "User has already been added."
     
         if error is None:
-            db.execute('INSERT INTO players (steamID, author, addedDate, banned) VALUES (?, ?, ?, ?)', 
-                (userId, str(ctx.author.id), datetime.datetime.now(), 0))
+            db.execute('INSERT INTO players (steam_id, author, add_date, banned, guild_id) VALUES (?, ?, ?, ?, ?)', 
+                (userId, str(ctx.author.id), datetime.datetime.now(), 0, ctx.guild.id))
             db.commit()
-            #await ctx.message.delete() 
             await ctx.send(f"Player with ID {userId} added to tracking list.")
             await called_once_a_day_vac(userId)
         else:
@@ -100,13 +130,27 @@ async def vac(ctx, input=None, playerLink=None):
     else:
         await ctx.send("You must supply a community profile URL.")
 
+# Add a player to the tracking list for ban checking
+@bot.command()
+@commands.has_guild_permissions(manage_guild=True)
+async def channel(ctx, channelId):
+    db = get_db()
+    if channelId is None:
+        await ctx.send("You need to supply a channel ID. Enable developer mode -> Right click on desired channel -> Copy ID")
+        return
+    db.execute('UPDATE guilds SET alert_channel = ? WHERE id = ?', (channelId, ctx.guild.id)) 
+    db.commit()
+    await ctx.send("Alert channel updated successfully.")
+
+
+## Functions
 # Show a list of players
 async def showList(ctx, type):
     if type == "track":
-        playersDict = getAllPlayers(0)
+        playersDict = getAllPlayers(0, ctx.guild.id)
         embedTitle = "Currently Tracking Players:"
     elif type == "ban":
-        playersDict = getAllPlayers(1)
+        playersDict = getAllPlayers(1, ctx.guild.id)
         embedTitle = "Currently Banned Players:"
     playerEmbed = discord.Embed(title = embedTitle, colour = Color.purple())
     for playerId, values in playersDict.items():
@@ -117,12 +161,12 @@ async def showList(ctx, type):
 
 # Remove a player from tracking list
 async def removeFromList(ctx, playerLink):
-    db = database.get_db()
+    db = get_db()
     userId = SteamID.from_url(playerLink)
-    player = db.execute('SELECT author FROM players WHERE steamID = ?', (userId,)).fetchone()
+    player = db.execute('SELECT author FROM players WHERE steam_id = ? AND guild_id = ?', (userId, ctx.guild.id)).fetchone()
     if player:
         if int(player[0]) == ctx.message.author.id:
-            db.execute('DELETE FROM players WHERE steamID = ?', (userId,))
+            db.execute('DELETE FROM players WHERE steam_id = ? AND guild_id = ?', (userId, ctx.guild.id))
             db.commit()
             await ctx.channel.send(f"Player with ID {userId} removed from the tracking list.")
         else:
@@ -130,17 +174,36 @@ async def removeFromList(ctx, playerLink):
     else:
         await ctx.channel.send("This player does not exist in the tracking list.")
 
+# Return a list of all players
+def getAllPlayers(banned, guildId=None):
+    db = get_db()
+    playersDict = {}
+    if guildId:
+        playersDB = db.execute('SELECT steam_id, author, datetime(add_date), guild_id FROM players WHERE guild_id = ? AND banned = ?', (guildId, int(banned))).fetchall()    
+    else:
+        playersDB = db.execute('SELECT steam_id, author, datetime(add_date), guild_id FROM players WHERE banned = ?', (int(banned),)).fetchall()
+    for row in playersDB:
+            steamdID, author, addedDate, guild_id = row
+            conv_date = datetime.datetime.strptime(addedDate, "%Y-%m-%d %H:%M:%S").date().strftime("%d/%m/%Y")
+            playersDict[steamdID] = [author, conv_date, int(guild_id)]
+    return playersDict
+
+# Format date_time into D/MM/YYYY
+def formatDateTime(dateTime):
+    return datetime.datetime.strptime(dateTime, "%Y-%m-%d %H:%M:%S").date().strftime("%d/%m/%Y")
+
+
+## Scheduled Functions
 # Check if any tracked player is banned
 @tasks.loop(hours=24)
 async def called_once_a_day_vac(userId=None):
-    db = database.get_db()
-    channel = bot.get_channel(vacChannelId)
+    db = get_db()
     playersDict = {}
     if userId is None:
         playersDict = getAllPlayers(0)
     else:
-        playersDB = db.execute('SELECT author, datetime(addedDate) FROM players WHERE steamID = ?', (userId,)).fetchone()
-        playersDict[userId] = (playersDB[0], datetime.datetime.strptime(playersDB[1], "%Y-%m-%d %H:%M:%S").date().strftime("%d/%m/%Y"))
+        playersDB = db.execute('SELECT author, datetime(add_date), guild_id FROM players WHERE steam_id = ?', (userId,)).fetchone()
+        playersDict[userId] = (playersDB[0], datetime.datetime.strptime(playersDB[1], "%Y-%m-%d %H:%M:%S").date().strftime("%d/%m/%Y"), int(playersDB[2]))
 
     for playerId, values in playersDict.items():
         player = json.loads(requests.get(f'http://api.steampowered.com/ISteamUser/GetPlayerBans/v1/?key={steamToken}&steamids={playerId}').text)['players'][0]
@@ -149,6 +212,7 @@ async def called_once_a_day_vac(userId=None):
         vacAmount = player['NumberOfVACBans']
         gameAmount = player['NumberOfGameBans']
         banDay = player['DaysSinceLastBan']
+        alertChannel = db.execute('SELECT alert_channel FROM guilds WHERE id = ?', (values[2],)).fetchone()[0]
         banProfile = f"https://steamcommunity.com/profiles/{playerId}"
         if vacBan:
             banField = "VAC"
@@ -162,30 +226,18 @@ async def called_once_a_day_vac(userId=None):
             playerEmbed.add_field(name="Total Bans", value=vacAmount)
             playerEmbed.add_field(name="Added On", value=values[1])
             playerEmbed.add_field(name="Added By", value=f"<@!{values[0]}>")
+            channel = bot.get_channel(int(alertChannel))
             await channel.send(embed=playerEmbed)
-            db.execute('UPDATE players SET banned = 1 WHERE steamID = ?', (playerId,))        
+            db.execute('UPDATE players SET banned = 1 WHERE steam_id = ? AND guild_id = ?', (playerId, values[2]))        
             db.commit()
 
-# Return a list of all players
-def getAllPlayers(banned):
-    db = database.get_db()
-    playersDict = {}
-    playersDB = db.execute('SELECT steamID, author, datetime(addedDate) FROM players WHERE banned = ?', (int(banned),)).fetchall()
-    for row in playersDB:
-        steamdID, author, addedDate = row
-        conv_date = datetime.datetime.strptime(addedDate, "%Y-%m-%d %H:%M:%S").date().strftime("%d/%m/%Y")
-        playersDict[steamdID] = [author, conv_date]
-    return playersDict
-
-# Format date_time into D/MM/YYYY
-def formatDateTime(dateTime):
-    return datetime.datetime.strptime(dateTime, "%Y-%m-%d %H:%M:%S").date().strftime("%d/%m/%Y")
 
 # Daily ban checker
 @called_once_a_day_vac.before_loop
 async def before():
     await bot.wait_until_ready()
     print("Finished waiting for daily check")
+
 
 # Main run
 called_once_a_day_vac.start()
--- database.py
+++ database.py
@@ -2,7 +2,7 @@ import sqlite3
 
 def get_db():
     db = sqlite3.connect(
-        'database.db',
+        'csgobot.db',
         detect_types=sqlite3.PARSE_DECLTYPES
     )
     return db
@@ -13,6 +13,10 @@ def close_db(db):
 
 def init_db():
     db = get_db()
+    db.execute('DROP TABLE IF EXISTS guilds')
     db.execute('DROP TABLE IF EXISTS players')
-    db.execute('CREATE TABLE players (id INTEGER PRIMARY KEY AUTOINCREMENT, steamID INT NOT NULL, author TEXT NOT NULL, addedDate TEXT NOT NULL, banned BOOL NOT NULL)')
+    db.execute('CREATE TABLE guilds (id INTEGER PRIMARY KEY AUTOINCREMENT, alert_channel TEXT NOT NULL, add_date TEXT NOT NULL)')
+    db.execute('''CREATE TABLE players (id INTEGER PRIMARY KEY AUTOINCREMENT, steam_id INT NOT NULL, author TEXT NOT NULL, 
+        add_date TEXT NOT NULL, banned BOOL NOT NULL, guild_id INT NOT NULL,
+        FOREIGN KEY (guild_id) REFERENCES guilds (id))''')
     db.close()
